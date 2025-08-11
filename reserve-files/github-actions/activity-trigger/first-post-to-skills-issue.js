a463d5cabff1f905843af3b2f8338d02a9c1ace5
HackforLABot <64623632+HackforLABot@users.noreply.github.com>
2025-08-10 20:17:53 -0700

    Update first-post-to-skills-issue.js

--- reserve-files/github-actions/activity-trigger/first-post-to-skills-issue.js
+++ reserve-files/github-actions/activity-trigger/first-post-to-skills-issue.js
@@ -30,7 +30,7 @@ async function firstPostToSkillsIssue({g, c}) {
 
 
     try {
-        const csvPath = 'github-actions/activity-trigger/member_activity_history_bot_8.csv';
+        const csvPath = 'github-actions/activity-trigger/member_activity_history_bot_7.csv';
 
         const csvContent = fs.readFileSync(csvPath, 'utf-8');
 
