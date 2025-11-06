79952d5e9e0c1d57d3cda621cbfed25d95d810df
t-will-gillis <339.colorado.dev@gmail.com>
2025-11-06 10:01:21 -0800

    Merge reserve-files/get-la-timestamp.js into shared/manage-issue-timeline.js

--- shared/manage-issue-timeline.js
+++ shared/manage-issue-timeline.js
@@ -41,3 +41,33 @@ async function getIssueTimeline(github, context, issueNum) {
 }
 
 module.exports = getIssueTimeline;
+/**
+ * Returns the current date and time in Los Angeles time (PST/PDT)
+ * formatted as a string.
+ *
+ * The output format is: `YYYY/MM/DD HH:MM TZ`, where `TZ` is either
+ * PST or PDT depending on daylight saving time.
+ *
+ * @returns {string} Formatted timestamp in Los Angeles local time.
+ *
+ * @example
+ * const timestamp = getLATimestamp();
+ * console.log(timestamp); // "2025/09/02 12:38 PDT"
+ */
+function getLATimestamp() {
+  // Create notification time string in PST/PDT
+  return new Date().toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+    timeZoneName: "short",
+  });
+}
+
+module.exports = getLATimestamp;
+
+C:\Users\twill\Desktop\projects\003_hackforla\automate-the-org\reserve-files\get-la-timestamp.js
