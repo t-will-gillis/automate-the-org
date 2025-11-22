30a29138da0853b20fb2d9af0748185aafc1a05d
t-will-gillis <339.colorado.dev@gmail.com>
2025-11-22 12:40:23 -0800

    edits to manageIssueTimeline

--- shared/manage-issue-timeline.js
+++ shared/manage-issue-timeline.js
@@ -2,36 +2,40 @@ const { logger } = require('./format-log-messages');
 
 /**
  * Function that returns the timeline of an issue
- * @param {Object} github                 - github object from actions/github-script
+ * @param {Object} github                 - GitHub object from actions/github-script
  * @param {Object} context                - context object from actions/github-script
  * @param {Number} issueNum               - the issue number
- * @returns {RObject} timelineArray        - an array containing the timeline of issue events
+* @returns {Array<Object>} timelineArray  - an array containing the timeline of issue events
  */
-async function getTimeline(issueNum, github, context) {
+async function getIssueTimeline(issueNum, github, context) {
   let timelineArray = [];
   let page = 1;
+
   while (true) {
     try {
-      const results = await github.rest.issues.listEventsForTimeline({
+      // https://docs.github.com/en/rest/issues/timeline?apiVersion=2022-11-28#list-timeline-events-for-an-issue
+      const results = await github.request('GET /repos/{owner}/{repo}/issues/{issue_number}/timeline', {
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: issueNum,
         per_page: 100,
         page: page,
       });
-      if (results.data.length) {
-        timelineArray = timelineArray.concat(results.data);
-      } else {
+
+      // If the API call returns an empty array, break out of loop- there is no additional data.
+      // Else if data is returned, push it to `timelineArray` and increase the page number (`page`)
+      if (!results.data.length) {
         break;
+      } else {
+        timelineArray.push(...results.data);
+        page++;
       }
     } catch (err) {
-      console.log(err);
-      continue;
-    } finally {
-      page++;
+      logger.error(`Error fetching issue timeline (page ${page}):`, err);
+      break;
     }
   }
   return timelineArray;
 }
 
-module.exports = getTimeline;
+module.exports = { getIssueTimeline };
