9f7cf21547b0532f505c8b964f26ce573fe7dbc7
t-will-gillis <339.colorado.dev@gmail.com>
2025-11-25 13:22:16 -0800

    build v0.5.1

--- add-update-label-weekly/dist/index.js
+++ add-update-label-weekly/dist/index.js
@@ -8,7 +8,7 @@
 const { logger } = __nccwpck_require__(2515);
 const queryIssueInfo = __nccwpck_require__(1563);
 const findLinkedIssue = __nccwpck_require__(8733);
-const { setLocalTime, getIssueTimeline } = __nccwpck_require__(5616);
+const { setLocalTime, getIssueTimeline } = __nccwpck_require__(2763);
 const { addLabels, removeLabels } = __nccwpck_require__(4603);
 const minimizeIssueComment = __nccwpck_require__(7688);
 
@@ -34598,6 +34598,91 @@ module.exports = { addLabels, removeLabels }
 
 /***/ }),
 
+/***/ 2763:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+// Import modules
+const { logger } = __nccwpck_require__(2515);
+
+/**
+ * Default returns the current date and time in Los Angeles time (PST/PDT)
+ * formatted as a string.
+ *
+ * The output format is: `YYYY/MM/DD HH:MM TZ`, where `TZ` is either
+ * PST or PDT depending on daylight saving time.
+ *
+ * @param {String} datetime                         - The date and time string from the event
+ * @param {String} [timezone='America/Los_Angeles'] - Optional IANA timezone string
+ * @returns {String}                                - Formatted date and time string for timezone
+ */
+function setLocalTime(datetime, timezone = 'America/Los_Angeles') {
+  // Validate timezone input
+  try {
+    Intl.DateTimeFormat(undefined, { timeZone: timezone });
+  } catch (e) {
+    logger.warn(`Invalid timezone specified: '${timezone}', defaulting to 'America/Los_Angeles'`);
+    timezone = 'America/Los_Angeles';
+  }
+
+  // Create notification time string in PST/PDT
+  return new Date(datetime).toLocaleString("en-US", {
+    timeZone: timezone,
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZoneName: "short",
+  });
+}
+
+
+
+/**
+ * Function that returns the timeline of an issue
+ * @param {Object} github                 - GitHub object from actions/github-script
+ * @param {Object} context                - context object from actions/github-script
+ * @param {Number} issueNum               - the issue number
+ * @returns {Array<Object>} timelineArray - an array containing the timeline of issue events
+ */
+async function getIssueTimeline(github, context, issueNum) {
+
+  let timelineArray = [];
+  let page = 1;
+
+  while (true) {
+    try {
+      // https://docs.github.com/en/rest/issues/timeline?apiVersion=2022-11-28#list-timeline-events-for-an-issue
+      const results = await github.request('GET /repos/{owner}/{repo}/issues/{issue_number}/timeline', {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issueNum,
+        per_page: 100,
+        page: page,
+      });
+
+      // If the API call returns an empty array, break out of loop- there is no additional data.
+      // Else if data is returned, push it to `timelineArray` and increase the page number (`page`)
+      if (!results.data.length) {
+        break;
+      } else {
+        timelineArray.push(...results.data);
+        page++;
+      }
+    } catch (err) {
+      logger.error(`Error fetching issue timeline (page ${page}):`, err);
+      break;
+    }
+  }
+  return timelineArray;
+}
+
+module.exports = { setLocalTime, getIssueTimeline };
+
+
+/***/ }),
+
 /***/ 1563:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -34915,14 +35000,6 @@ async function resolveLabels({
 }
 
 module.exports = { resolve:resolveLabels };
-
-/***/ }),
-
-/***/ 5616:
-/***/ ((module) => {
-
-module.exports = eval("require")("../shared/get-issue-timeline");
-
 
 /***/ }),
 
